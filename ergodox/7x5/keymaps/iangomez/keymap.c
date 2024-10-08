11f5b46363677b4b4c023a7b9aa4bb75830996fd
Ian Gomez <ian.nicolasgomez@gmail.com>
2024-10-07 23:27:47 -0700

    iandox debug

--- ergodox/7x5/keymaps/iangomez/keymap.c
+++ ergodox/7x5/keymaps/iangomez/keymap.c
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT_7x5(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,       _______,                    _______,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,       _______,                    _______,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
-    ALT_F9,         LT1_A,          LT2_S,          LT3_D,          LT4_F,          KC_G,                                                   KC_H,           KC_J,           KC_K,           KC_L,           LT4_CLN,        KC_QUOTE,       
-    KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,       _______,                    _______,        KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLASH,       KC_RALT,   
+    ALT_F9,         LT1_A,          LT2_S,          LT3_D,          LT4_F,          KC_G,       _______,                    _______,        KC_H,           KC_J,           KC_K,           KC_L,           LT4_CLN,        KC_QUOTE,       
+    KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                   KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLASH,       KC_RALT,   
     _______,        _______,        _______,        _______,        _______,                                                                                _______,        _______,        _______,        _______,        _______,   
                                                                     KC_SPACE,       KC_LSFT,                                                KC_BSPC,        KC_ENTER,
                                                                     _______,        KC_LCTL,                                                KC_LGUI,        _______,
--- ergodox/config.h
+++ ergodox/config.h
@@ -4,6 +4,9 @@
 #define MASTER_LEFT                 // which side is plugged in
 #define SERIAL_USART_FULL_DUPLEX    // Enable full duplex operation mode
 
+#define SERIAL_TX_PIN 0  // Set your TX pin
+#define SERIAL_RX_PIN 1  // Set your RX pin
+
 #define RGBLIGHT_SLEEP /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 
 #define SPLIT_TRANSPORT_SYNC_ENABLE
